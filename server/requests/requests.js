f06c6ff32fe1aa620d641ac98c7fce5c975ea7ee
fjodor-rybakov <fedryb@mail.ru>
2018-09-30 17:15:13 +0300

    change signup server request

--- server/requests/requests.js
+++ server/requests/requests.js
@@ -18,7 +18,7 @@ module.exports = (server, database) => {
         return true;
     }
 
-    function isUserExist(email, next) {
+    /*function isUserExist(email, next) {
         const sql = `SELECT * FROM user WHERE email = '${email}'`;
         database.query(sql, function (err, result) {
             if (err) {
@@ -55,5 +55,43 @@ module.exports = (server, database) => {
         } else {
             res.send("email is already exist")
         }
-    });
+    });*/
+
+    server.post("/v1/signUp", getSignUp);
+
+    function getSignUp(req, res, next) {
+        const data = JSON.parse(req.body);
+        if (!isset(data.email, data.password)) {
+            return next(new errs.InvalidArgumentError("Not enough body data"));
+        }
+        addUser(data, next)
+            .then(() => {
+                res.send("success");
+            })
+            .catch(() => {
+                res.send("email is already exist");
+            });
+    }
+
+    function addUser(data, next) {
+        return new Promise(async (resolve, reject) => {
+            let sql = `SELECT * FROM user WHERE email = '${data.email}'`;
+            database.query(sql, function (err, result) {
+                if (err) {
+                    return next(new errs.BadGatewayError(err));
+                }
+                if (result.length !== 0) {
+                    return reject(false);
+                }
+            });
+
+            sql = `INSERT INTO user VALUES (null, '', '', 1, '', '${data.password}', '${data.email}', '')`;
+            database.query(sql, function (err) {
+                if (err) {
+                    return next(new errs.BadGatewayError(err));
+                }
+                return resolve(true);
+            });
+        });
+    }
 };
